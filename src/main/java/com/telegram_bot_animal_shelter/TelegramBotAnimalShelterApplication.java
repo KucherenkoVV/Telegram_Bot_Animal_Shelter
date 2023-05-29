ed1b0f22ae1d2edcdc4bf518cae665165037ab7c
Hw2 <Kucherenko.vv@mail.ru>
2023-05-29 21:58:47 +0800

    init commit

---
+++ src/main/java/com/telegram_bot_animal_shelter/TelegramBotAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package com.telegram_bot_animal_shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotAnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telegram_bot_animal_shelter/TelegramBotAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram_bot_animal_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
