75a44b81fc41159c850fda45ac54a8415e969664
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-06-07 11:19:46 +0400

    1 commit

--- src/main/java/com/telegram_bot_animal_shelter/service/CatService.java
+++ src/main/java/com/telegram_bot_animal_shelter/service/CatService.java
@@ -1,4 +1,9 @@
 package com.telegram_bot_animal_shelter.service;
+/**
+ * class of CatService
+ * @author Dmitriy Volkov
+ * @version 0.0.1
+ */
 
 import com.telegram_bot_animal_shelter.exceptions.CatNotFoundException;
 import com.telegram_bot_animal_shelter.model.Cat;
