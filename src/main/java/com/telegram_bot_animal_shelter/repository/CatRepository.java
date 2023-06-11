e094328d8d0d70ff7cd7bb27e7b624dc2d4594c6
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-11 15:10:54 +0700

    7 commit

--- src/main/java/com/telegram_bot_animal_shelter/repository/CatRepository.java
+++ src/main/java/com/telegram_bot_animal_shelter/repository/CatRepository.java
@@ -1,4 +1,13 @@
 package com.telegram_bot_animal_shelter.repository;
 
-public interface CatRepository {
+import com.telegram_bot_animal_shelter.entity.Cat;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Интерфейс CatRepository
+ * @author Vladislav Zhitar
+ * @version 1.0.0
+ */
+@Repository
+public interface CatRepository extends JpaRepository<Cat, Long> {
 }
