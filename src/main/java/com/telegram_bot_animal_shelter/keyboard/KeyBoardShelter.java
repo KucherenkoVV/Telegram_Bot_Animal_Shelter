027b7c91a141ac5bbe2c767735870743864eed68
Hw2 <Kucherenko.vv@mail.ru>
2023-06-11 21:13:08 +0800

    Add methods KeyBoardShelter

--- src/main/java/com/telegram_bot_animal_shelter/keyboard/KeyBoardShelter.java
+++ src/main/java/com/telegram_bot_animal_shelter/keyboard/KeyBoardShelter.java
@@ -10,9 +10,9 @@ import com.telegram_bot_animal_shelter.listener.TelegramBotUpdateListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class KeyBoardShelter {
 
     @Autowired
@@ -20,20 +20,28 @@ public class KeyBoardShelter {
 
     private static final Logger logger = LoggerFactory.getLogger(TelegramBotUpdateListener.class);
 
-    public void chooseAnimals(long chatId) {
-        logger.info("Method sendMessage has been run: {}, {}", chatId, "Вызвано меню выбора ");
+    /**
+     * Display menu with a basic set of buttons.
+     * Class methods used: {@code ReplyKeyboardMarkup}
+     * Class methods used: {@code SendMessage}
+     * @param chatId
+     * @see KeyBoardShelter
+     */
+    public void sendMenu(long chatId) {
+        logger.info("Starting main menu: {}, {}", chatId, "Вызвано основное меню");
 
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(new KeyboardButton("Кошачий приют"));
-        replyKeyboardMarkup.addRow(new KeyboardButton("Собачий приют"));
-        buttonShelter(replyKeyboardMarkup, chatId, "Выберите приют:");
-    }
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton("Информация о возможностях бота"),
+                new KeyboardButton("Узнать информацию о приюте"));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Как взять питомца из приюта"),
+                new KeyboardButton("Прислать отчет о питомце"));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"));
 
-    public void buttonShelter(ReplyKeyboardMarkup replyKeyboardMarkup, Long chatId, String text) {
         replyKeyboardMarkup.resizeKeyboard(true);
         replyKeyboardMarkup.oneTimeKeyboard(false);
         replyKeyboardMarkup.selective(false);
 
-        SendMessage request = new SendMessage(chatId, text)
+        SendMessage request = new SendMessage(chatId, "Добро пожаловать, в наш приют")
                 .replyMarkup(replyKeyboardMarkup)
                 .parseMode(ParseMode.HTML)
                 .disableWebPagePreview(true);
@@ -42,8 +50,65 @@ public class KeyBoardShelter {
         if (!sendResponse.isOk()) {
             int codeError = sendResponse.errorCode();
             String description = sendResponse.description();
-            logger.info("code of error: {}", codeError);
+            logger.info("error code: {}", codeError);
             logger.info("description -: {}", description);
         }
+    }
+
+    /**
+     * Display menu with information about the shelter.
+     * Class methods used: {@code ReplyKeyboardMarkup}
+     * Class methods used: {@code SendMessage}
+     * @param chatId
+     * @see KeyBoardShelter
+     */
+    public void sendMenuInfoShelter(long chatId) {
+        logger.info("Starting menu information about animal shelter: {}, {}", chatId, "Вызвано меню: Информация о приюте");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup2 = new ReplyKeyboardMarkup(new KeyboardButton("Статья"),
+                new KeyboardButton("Оставить контактные данные").requestContact(true));
+        repeatableMenu(chatId, replyKeyboardMarkup2);
+    }
+
+    /**
+     *
+     * @param chatId
+     * @param replyKeyboardMarkup2
+     */
+    private void repeatableMenu(long chatId, ReplyKeyboardMarkup replyKeyboardMarkup2) {
+        replyKeyboardMarkup2.addRow(new KeyboardButton("Позвать волонтера"),
+                new KeyboardButton("Вернуться в меню"));
+
+        replyKeyboardMarkup2.resizeKeyboard(true);
+        replyKeyboardMarkup2.oneTimeKeyboard(false);
+        replyKeyboardMarkup2.selective(false);
+
+        SendMessage request = new SendMessage(chatId, "Информацию о приюте")
+                .replyMarkup(replyKeyboardMarkup2)
+                .parseMode(ParseMode.HTML)
+                .disableWebPagePreview(true);
+        SendResponse sendResponse = telegramBot.execute(request);
+
+        if (!sendResponse.isOk()) {
+            int codeError = sendResponse.errorCode();
+            String description = sendResponse.description();
+            logger.info("error code: {}", codeError);
+            logger.info("description -: {}", description);
+        }
+    }
+
+    /**
+     * Display a menu with information about how to take a pet.
+     * Class methods used: {@code ReplyKeyboardMarkup}
+     * Class methods used: {@code SendMessage}
+     * @param chatId
+     * @see KeyBoardShelter
+     */
+    public void sendMenuTakeAnimal(long chatId) {
+        logger.info("Starting menu about take animal from shelter: {}, {}", chatId, "вызвали Как взять питомца из приюта");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup3 = new ReplyKeyboardMarkup(new KeyboardButton("Кидает на статью"),
+                new KeyboardButton("Оставить контактные данные"));
+        repeatableMenu(chatId, replyKeyboardMarkup3);
     }
 }
