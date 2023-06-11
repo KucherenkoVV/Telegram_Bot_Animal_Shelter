550ea658b9f3646bad24c86322815a436dedcbfd
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-11 14:44:43 +0700

    5 commit

--- src/main/java/com/telegram_bot_animal_shelter/exceptions/DogNotFoundException.java
+++ src/main/java/com/telegram_bot_animal_shelter/exceptions/DogNotFoundException.java
@@ -1,4 +1,12 @@
 package com.telegram_bot_animal_shelter.exceptions;
 
-public class DogNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DogNotFoundException extends RuntimeException {
+
+    public DogNotFoundException() {
+        super("Собака не найдена!");
+    }
 }
--- src/main/java/com/telegram_bot_animal_shelter/exceptions/ReportNotFoundException.java
+++ src/main/java/com/telegram_bot_animal_shelter/exceptions/ReportNotFoundException.java
@@ -1,4 +1,12 @@
 package com.telegram_bot_animal_shelter.exceptions;
 
-public class ReportNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ReportNotFoundException extends RuntimeException {
+
+    public ReportNotFoundException() {
+        super("Отчёт не найден!");
+    }
 }
