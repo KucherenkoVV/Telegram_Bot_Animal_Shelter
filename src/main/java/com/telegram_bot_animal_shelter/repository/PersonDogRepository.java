4e92373b2a72276e810dc49b858d983d840fcdcb
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-11 15:10:54 +0700

    7 commit

--- src/main/java/com/telegram_bot_animal_shelter/repository/PersonDogRepository.java
+++ src/main/java/com/telegram_bot_animal_shelter/repository/PersonDogRepository.java
@@ -1,17 +1,16 @@
 package com.telegram_bot_animal_shelter.repository;
 
 import com.telegram_bot_animal_shelter.model.PersonDog;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 /**
- * Interface PersonDogRepository
- * @author
+ * Интерфейс PersonDogRepository
+ * @author Zhitar Vladislav
  * @version 1.0.0
  */
 @Repository
 public interface PersonDogRepository extends JpaRepository<PersonDog, Long> {
 
-}
+    Set<PersonDog> findByChatId(Long chatId);
