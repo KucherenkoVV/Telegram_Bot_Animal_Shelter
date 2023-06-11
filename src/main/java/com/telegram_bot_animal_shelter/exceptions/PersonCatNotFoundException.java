73c168afa552a2ecf2ec7122982ad37fc75b4fd1
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-11 14:44:43 +0700

    5 commit

--- src/main/java/com/telegram_bot_animal_shelter/exceptions/PersonCatNotFoundException.java
+++ src/main/java/com/telegram_bot_animal_shelter/exceptions/PersonCatNotFoundException.java
@@ -1,4 +1,12 @@
 package com.telegram_bot_animal_shelter.exceptions;
 
-public class PersonCatNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PersonCatNotFoundException extends RuntimeException {
+
+    public PersonCatNotFoundException() {
+        super("Усыновитель кота не найден!");
+    }
 }
