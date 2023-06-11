87bd004e5dd6adb19a402b3abe1f6f82b83eb4b8
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-11 14:34:29 +0700

    4 commit

--- src/main/java/com/telegram_bot_animal_shelter/keyboard/KeyBoardShelter.java
+++ src/main/java/com/telegram_bot_animal_shelter/keyboard/KeyBoardShelter.java
@@ -20,15 +20,69 @@ public class KeyBoardShelter {
 
     private static final Logger logger = LoggerFactory.getLogger(TelegramBotUpdateListener.class);
 
+    /** Метод, позволяющий выбрать приют;
+     * @param chatId
+     */
     public void chooseAnimals(long chatId) {
         logger.info("Method sendMessage has been run: {}, {}", chatId, "Вызвано меню выбора ");
 
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(new KeyboardButton("Кошачий приют"));
         replyKeyboardMarkup.addRow(new KeyboardButton("Собачий приют"));
-        buttonShelter(replyKeyboardMarkup, chatId, "Выберите приют:");
+       returnsAResponseToTheUser(replyKeyboardMarkup, chatId, "Выберите приют:");
     }
 
-    public void buttonShelter(ReplyKeyboardMarkup replyKeyboardMarkup, Long chatId, String text) {
+    /** Метод, позволяющий вызват основное меню;
+     * @param chatId
+     */
+    public void mainMenu(long chatId) {
+        logger.info("Method sendMessage has been run: {}, {}", chatId, "Вызвано основное меню ");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton("Информация о возможностях бота"),
+                new KeyboardButton("Узнать информацию о приюте"));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Как взять питомца из приюта"),
+                new KeyboardButton("Прислать отчет о питомце"));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"));
+
+        returnsAResponseToTheUser(replyKeyboardMarkup, chatId, "Главное меню");
+    }
+
+    /**
+     * Метод, позволяющий получить информацию о приюте;
+     * @param chatId
+     */
+    public void getInformationAboutTheShelter(long chatId) {
+        logger.info("Method sendMenuInfoShelter has been run: {}, {}", chatId, "Вызвали ~Информация о приюте~");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(new KeyboardButton("Информация о приюте"),
+                new KeyboardButton("Оставить контактные данные").requestContact(true));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"),
+                new KeyboardButton("Вернуться в меню"));
+        returnsAResponseToTheUser(replyKeyboardMarkup, chatId, "Информация о приюте");
+    }
+
+    /**
+     * Метод, позволяющий взять питомца из приюта;
+     * @param chatId
+     */
+    public void sendMenuTakeAnimal(long chatId) {
+        logger.info("Method sendMenuTakeAnimal has been run: {}, {}", chatId, "вызвали ~Как взять питомца из приюта~");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton("Советы и рекомендации"),
+                new KeyboardButton("Оставить контактные данные").requestContact(true));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"),
+                new KeyboardButton("Вернуться в меню"));
+        returnsAResponseToTheUser(replyKeyboardMarkup, chatId, "Как взять питомца из приюта");
+    }
+
+    /** Метод, возращает ответ позьзователю;
+     * @param replyKeyboardMarkup
+     * @param chatId
+     * @param text
+     */
+
+    public void returnsAResponseToTheUser(ReplyKeyboardMarkup replyKeyboardMarkup, Long chatId, String text) {
         replyKeyboardMarkup.resizeKeyboard(true);
         replyKeyboardMarkup.oneTimeKeyboard(false);
         replyKeyboardMarkup.selective(false);
