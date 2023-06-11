ad29eae79fb7fcbc0accbfaf18ea23ebc1695334
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-11 15:10:54 +0700

    7 commit

--- src/main/java/com/telegram_bot_animal_shelter/repository/ReportRepository.java
+++ src/main/java/com/telegram_bot_animal_shelter/repository/ReportRepository.java
@@ -1,4 +1,20 @@
 package com.telegram_bot_animal_shelter.repository;
 
-public interface ReportRepository {
+import com.telegram_bot_animal_shelter.entity.Report;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+/**
+ * Интерфейс ReportRepository
+ * @author Zhitar Vladislav
+ * @version 1.0.0
+ */
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    Set<Report> findListByChatId(Long id);
+
+    Report findByChatId(Long id);
+
 }
