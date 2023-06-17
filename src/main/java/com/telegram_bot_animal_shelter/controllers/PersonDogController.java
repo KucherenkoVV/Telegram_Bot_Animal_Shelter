048fe1a2466092115dbc86a9405808fd08bea12e
R2D2VLAD <zitarvlad11@gmail.com>
2023-06-17 17:49:39 +0700

    Commit 12

--- src/main/java/com/telegram_bot_animal_shelter/controllers/PersonDogController.java
+++ src/main/java/com/telegram_bot_animal_shelter/controllers/PersonDogController.java
@@ -1,4 +1,115 @@
 package com.telegram_bot_animal_shelter.controllers;
 
+import com.telegram_bot_animal_shelter.model.PersonDog;
+import com.telegram_bot_animal_shelter.service.impl.PersonDogServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+/**
+ * Class of PersonDogController.
+ *
+ * @author Kucherenko.vv
+ * @version 0.0.1
+ */
+@RestController
+@RequestMapping("person-dog")
 public class PersonDogController {
+
+    private final PersonDogServiceImpl personDogServiceImpl;
+
+    public PersonDogController(PersonDogServiceImpl personDogServiceImpl) {
+        this.personDogServiceImpl = personDogServiceImpl;
+    }
+
+    @Operation(summary = "Получение пользователя, усыновителя собаки,  по id",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Пользователь, усыновитель собаки, найден по id",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = PersonDog.class)
+                            )
+                    )
+            },
+            tags = "PersonDog"
+    )
+    @GetMapping("/{id}")
+    public PersonDog getById(@Parameter(description = "PersonDog id") @PathVariable Long id) {
+        return this.personDogServiceImpl.getByIdPersonDog(id);
+    }
+
+    @Operation(summary = "Создание пользователя, усыновителя собаки",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Пользователь, усыновитель собаки, найден",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = PersonDog.class)
+                    )
+            ),
+            tags = "PersonDog"
+    )
+    @PostMapping
+    public PersonDog save(@RequestBody PersonDog personDog) {
+        return this.personDogServiceImpl.addPersonDog(personDog);
+    }
+
+    @Operation(summary = "Изменение данных пользователя, усыновителя кота",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Данные пользователя, усыновителя собаки, изменены",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = PersonDog.class)
+                    )
+            ),
+            tags = "PersonDog"
+    )
+    @PutMapping
+    public PersonDog update(@RequestBody PersonDog personDog) {
+        return this.personDogServiceImpl.updatePersonDog(personDog);
+    }
+
+    @Operation(summary = "Удаление пользователей по id",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Пользователь, удаленный по id",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = PersonDog.class)
+                            )
+                    )
+            },
+            tags = "PersonDog"
+    )
+    @DeleteMapping("/{id}")
+    public void remove(@Parameter(description = "PersonDog id") @PathVariable Long id) {
+        this.personDogServiceImpl.removeByIdPersonDog(id);
+    }
+
+    @Operation(summary = "Просмотр всех пользователей",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Все пользователи, либо определенные пользователи по chat_id",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = PersonDog.class)
+                            )
+                    )
+            },
+            tags = "PersonDog"
+    )
+    @GetMapping("/all")
+    public Collection<PersonDog> getAll() {
+        return this.personDogServiceImpl.getAllPersonDog();
+    }
 }
+
--- src/test/java/com/telegram_bot_animal_shelter/controller/CatControllerTest.java
+++ src/test/java/com/telegram_bot_animal_shelter/controller/CatControllerTest.java
@@ -1,4 +1,142 @@
 package com.telegram_bot_animal_shelter.controller;
 
-public class CatControllerTest {
+import com.telegram_bot_animal_shelter.controllers.CatController;
+import com.telegram_bot_animal_shelter.model.Cat;
+import com.telegram_bot_animal_shelter.service.CatService;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Class for testing CatController
+ * @see CatService
+ * @author Artem Alekseev
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class CatControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CatService catService;
+
+    /**
+     * Test for <b>getById()</b> method in CatController
+     * <br>
+     * Mockito: when <b>CatService::getById()</b> method called, returns <b>cat</b> object
+     * @throws Exception
+     */
+    @Test
+    void getByIdCat() throws Exception {
+        Cat cat = new Cat();
+        cat.setId(1L);
+
+        when(catService.getByIdCat(anyLong())).thenReturn(cat);
+
+        mockMvc.perform(
+                        get("/cat/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
+
+
+        verify(catService).getByIdCat(1L);
+    }
+
+    /**
+     * Test for <b>save()</b> method in CatController
+     * <br>
+     * Mockito: when <b>CatService::create()</b> method called, returns <b>cat</b> object
+     * @throws Exception
+     */
+    @Test
+    void addCat() throws Exception {
+        Cat cat = new Cat();
+        cat.setId(1L);
+        cat.setName("cat");
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", 1L);
+        userObject.put("name", "cat");
+
+        when(catService.addCat(cat)).thenReturn(cat);
+
+        mockMvc.perform(
+                        post("/cat")
+                                .content(userObject.toString())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(userObject.toString()));
+
+        verify(catService).addCat(cat);
+    }
+
+    /**
+     * Test for <b>update()</b> method in CatController
+     * <br>
+     * Mockito: when <b>CatService::update()</b> method called, returns <b>cat</b> object
+     * @throws Exception
+     */
+    @Test
+    void updateCat() throws Exception {
+        Cat cat = new Cat();
+        cat.setId(1L);
+        cat.setName("cat new");
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", 1L);
+        userObject.put("name", "cat new");
+
+        when(catService.updateCat(cat)).thenReturn(cat);
+
+        mockMvc.perform(
+                        put("/cat")
+                                .content(userObject.toString())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(userObject.toString()));
+
+        verify(catService).updateCat(cat);
+    }
+
+    /**
+     * Test for <b>remove()</b> method in CatController
+     * @throws Exception
+     */
+    @Test
+    void deleteCat() throws Exception {
+        mockMvc.perform(
+                        delete("/cat/{id}", 1))
+                .andExpect(status().isOk());
+        verify(catService).removeByIdCat(1L);
+    }
+
+    /**
+     * Test for <b>getAll()</b> method in CatController
+     * <br>
+     * Mockito: when <b>CatService::getAll()</b> method called, returns collection with <b>new Cat</b> object
+     * @throws Exception
+     */
+    @Test
+    void getAllCat() throws Exception {
+        when(catService.getAllCat()).thenReturn(List.of(new Cat()));
+
+        mockMvc.perform(
+                        get("/cat/all"))
+                .andExpect(status().isOk());
+    }
 }
